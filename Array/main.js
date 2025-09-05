7ef931cb41bed3a753f511e4e4829ea132a72270
xuantien0201 <xtien1812@gmail.com>
2025-09-05 19:14:49 +0700

    abc

--- Array/main.js
+++ Array/main.js
@@ -3,15 +3,15 @@
 
 // console.log(arr1)
 
-// //spread operator
+                 // //spread operator
 // let arr3 = [1, "abc", ...arr1, ...arr2];
 // console.log(arr3);
 
-// //array.prototype.fill
+                // //array.prototype.fill
 // let arr4 = new Array(5).fill(0);
 // console.log(arr4);
 
-// //array length
+                // //array length
 // let arr5 = [1, 2, 3];
 // console.log(arr5.length);
 
@@ -25,43 +25,43 @@
 //      function() { return "hello"; }];
 // console.log(arr6);
 
-// //for
+                    // //vòng lặp for
 // for (let i = 0; i < arr6.length; i++) {
 //     console.log(arr6[i]);
 // }
 
-// //for..of
+                    // //vòng lặp for..of
 // for (let item of arr6) {
 //     console.log(item);
 // }
 
-// //for..in
+                    // //for..in
 // for (let index in arr6) {
 //     console.log(index, arr6[index]);
 // }
 
-// 1 số phương thức cơ bản của array
-// arr.pop() : Lấy phần tử cuối cùng của mảng
+                    // 1 số phương thức cơ bản của array
+        // arr.pop() : Lấy phần tử cuối cùng của mảng
 // let arr1 = [1, 2, 3,];
 // console.log(arr1.pop());
 // console.log(arr1);
 
-// // arr.push() : Thêm phần tử vào cuối mảng
+        // // arr.push() : Thêm phần tử vào cuối mảng
 // let arr2 = [1, 2, 3];
 // console.log(arr2.push(4));
 // console.log(arr2);
 
-// // arr.shift() : Lấy phần tử đầu tiên của mảng
+        // // arr.shift() : Lấy phần tử đầu tiên của mảng
 // let arr3 = [1, 2, 3];
 // console.log(arr3.shift());
 // console.log(arr3);
 
-// // arr.unshift() : Thêm phần tử vào đầu mảng
+        // // arr.unshift() : Thêm phần tử vào đầu mảng
 // let arr4 = [1, 2, 3];
 // console.log(arr4.unshift(0));
 // console.log(arr4);
 
-// // length trong JS
+                    // // length trong JS
 // let arr1 = []
 
 // arr1[1000] = "abc"
@@ -75,7 +75,7 @@
 // // => 1 cách xóa phần tử trong arr là gán độ dài cho length
 // console.log(arr2)
 
-// Matrix trong JS
+                    // Matrix trong JS
 // let matrix = [
 //     [1,2,3],
 //     [4,5,6],
@@ -84,23 +84,72 @@
 // console.log(matrix)
 // console.log(matrix[0],[0])
 
-// // cách để duyệt từng phần tử trong matrix
+                // // cách để duyệt từng phần tử trong matrix
 // for(let i = 0; i < matrix.length; i++){
-//     for(let j = 0; j < matrix[i].length; i ++){
-//         console.log(matrix[i],[j])
+//     for(let j = 0; j < matrix[i].length; j++){
+//         console.log(matrix[i][j])
 //     }
 // } 
 // console.log(matrix.toString)
 
 
-//splice trong JS
+                //splice trong JS
 // let arr1 = [1,2,3,4,5]
 // arr1.splice(1, 2, 6, 7) // xóa 2 phần tử bắt đầu từ vị trí 1 và thêm 6, 7 vào
 // console.log(arr1.splice(1, 2, 6, 7) ) // in ra các phần tử bị xóa
 // console.log(arr1)
 
-//slice trong JS
-let arr2 = [1, 2, 3, 4, 5]
-let newArr = arr2.slice(1, 4) // cắt từ vị trí 1 đến 4 (không bao gồm vị trí 4)
-console.log(newArr)
-console.log(arr2)
+                //slice trong JS
+// let arr2 = [1, 2, 3, 4, 5]
+// let newArr = arr2.slice(1, 4) // cắt từ vị trí 1 đến 4 (không bao gồm vị trí 4)
+// console.log(newArr)
+// console.log(arr2)
+
+                //concat trong JS
+        //concat dùng để nối 2 mảng
+// let arr1 = [1, 2, 3]
+// let arr2 = arr1.concat([4, 5, 6])
+// console.log(arr2)
+
+                //indexOf trong JS
+        // dùng để tìm vị trí của phần tử trong mảng
+// let arr1 = [1, 2, 44, false, null, "abc"]
+// console.log(arr1.indexOf(false)) // trả về vị trí của phần tử trong mảng, nếu không tìm thấy trả về -1
+// console.log(arr1.indexOf("abc"))
+// console.log(arr1.indexOf(undefined))
+
+                // //lastIndexOf trong JS
+        // //tương tự indexOf nhưng tìm từ cuối mảng về đầu mảng
+// let arr2 = [1, 2, 3, 1, 2, 3]
+// console.log(arr2.lastIndexOf(2)) // trả về vị trí của phần tử trong mảng, nếu không tìm thấy trả về -1
+// console.log(arr2.lastIndexOf(4))
+
+                //includes trong JS
+        //dùng để kiểm tra phần tử có trong mảng hay không, trả về true hoặc false
+// let arr3 = [1, 2, 3, 4, 5]
+// console.log(arr3.includes(3)) // true
+// console.log(arr3.includes(6)) // false
+
+                //find và findIndex trong JS
+        //find trả về phần tử đầu tiên
+// let users = [
+//     {
+//         id: 1,
+//         name: "Nguyen Van A",
+//         age: 16
+//     },
+//     {
+//         id: 2,
+//         name: "Tran Thi B",
+//         age: 20
+//     },
+// ]
+// const findUser = users.find(function(user){
+//     return user.age > 18
+// })
+// console.log(findUser)
+
+// //findIndex trả về vị trí của phần tử đầu tiên
+// const findUserIndex = users.findIndex(function(user){
+//     return user.age > 18
+// })
---
+++ Object/main.js
@@ -0,0 +1,18 @@
+    // => có 2 cách để truy cập vào các thuộc tính của object
+            // 1. Dùng dấu chấm .
+            // 2. Dùng dấu ngoặc vuông []
+// let myComputer = {
+//     brand: "HP",
+//     model: "Pavilion",
+//     year: 2020,
+//     graphics: "NVIDIA",
+//     ram: "16GB",
+//     storage: "512GB SSD",
+// }
+// console.log(myComputer);
+// console.log(myComputer.brand)
+// console.log(myComputer.model)
+
+// console.log(myComputer["year"])
+// console.log(myComputer["graphics"])
+
